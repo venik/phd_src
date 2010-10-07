210e0ba2b14c3c4e3d73d7dc57b6aae756f8aa35
Alex Nikiforov <nikiforov.pub@gmail.com>
2010-10-08 02:33:03 +0400

    article: update pic

--- text/articles/lab_place/images/bpsk.m
+++ text/articles/lab_place/images/bpsk.m
@@ -1,9 +1,17 @@
+% signal
 x = -10*pi:0.01:10*pi;
 y = sin(x);
 x1 = ceil(length(x)*0.1);
 x2 = ceil(length(x)*0.9);
-
 y(x1:x2) = y(x1:x2) .* (-1) ;
-figure(1), plot(x,y), grid on;
+
+% bit
+y_bit = ones(length(y), 1);
+y_bit(x1:x2) = y_bit(x1:x2) .* (-1) ;
+
+
+figure(1), , grid on;
+subplot(2,1,1), plot(x,y), title('a)')
+subplot(2,1,2), plot(x, y_bit, 'r'), title('b)'), ylim([-1.5,1.5 ]);
 %figure (1, 'visible', 'off');
 print -deps 'bpsk.eps'
