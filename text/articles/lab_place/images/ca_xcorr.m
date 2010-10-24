cdf52eae761eee8570acfe871c38545195ee3175
Alex Nikiforov <nikiforov.pub@gmail.com>
2010-10-24 17:13:55 +0400

    article: add text about C/A code

---
+++ text/articles/lab_place/images/ca_xcorr.m
@@ -0,0 +1,10 @@
+% generate ca and xcorr(ca)
+
+addpath ("../../../../src/gnss/") ;
+
+ca = ca_generate_bits(19, 0);
+
+ca_corr = xcorr(ca);
+length(ca_corr)
+x = 0:2044;
+plot(x, ca_corr), grid on	; xlim([0,2044]), ylim([-100, 1050]);
