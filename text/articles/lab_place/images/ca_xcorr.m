7ffc82a1128e2ada07b6304e96c4b6a9e67f3cdb
Alex Nikiforov <nikiforov.pub@gmail.com>
2010-11-05 23:58:50 +0300

    article: some new text

--- text/articles/lab_place/images/ca_xcorr.m
+++ text/articles/lab_place/images/ca_xcorr.m
@@ -7,4 +7,6 @@ ca = ca_generate_bits(19, 0);
 ca_corr = xcorr(ca);
 length(ca_corr)
 x = 0:2044;
-plot(x, ca_corr), grid on	; xlim([0,2044]), ylim([-100, 1050]);
+plot(x, ca_corr), grid on; xlim([0,2044]), ylim([-100, 1050]);
+figure (1, 'visible', 'on');
+print -deps 'ca_xcorr.eps'
