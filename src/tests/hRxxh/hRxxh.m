cff862eecb3e93215963d220bce6621d60139357
Alex Nikiforov <nikiforov.pub@gmail.com>
2010-12-13 00:40:33 +0300

    test: h'Rxxh

--- src/tests/hRxxh/hRxxh.m
+++ src/tests/hRxxh/hRxxh.m
@@ -1,10 +1,14 @@
 % noise energy estimation routine
 clc, clear all;
 sigma_n = .4 ;
-N_filt = 128 ;
 drw_pts = 500 ;
 
-N = 1024*128 ;
+%N_filt = 128 ;
+%N = 1024*128 ;
+N = 16368 ;
+N_filt = 2 ;
+
+
 xp = cos(2*pi/N_filt*(0:(N_filt-1))).' ;
 xp(xp>0) = 2 ;
 xp(xp<0) = -3 ;
@@ -27,14 +31,15 @@ hold on, plot(1+offs:(length(y(1:drw_pts))+offs),y(1:drw_pts))
 %end
 f = conv(h, y) ;
 
-X = zeros(N_filt,N_filt) ;
-for k=1:N_filt
-    X(:,k) = [xp(k:end);xp(1:k-1)] ;
-end
-Rxx = X'*X/N_filt ;
-Rxx = Rxx/Rxx(1,1) ;
+%X = zeros(N_filt,N_filt) ;
+%for k=1:N_filt
+    %X(:,k) = [xp(k:end);xp(1:k-1)] ;			% by mao
+%    X(:,k) = [xp(k:-1:2);xp(1:end-k+1)] ;		% by me
+%end
+%Rxx = X'*X/N_filt ;
+%Rxx = Rxx/Rxx(1,1) ;
 
-hold on, plot(f(1:drw_pts)/sqrt(h'*Rxx*h),'r-','Linewidth',2)
+%hold on, plot(f(1:drw_pts)/sqrt(h'*Rxx*h),'r-','Linewidth',2)
 
 % Es = h(end:-1:1)'*Rxx*h(end:-1:1) ;
 % En = h(end:-1:1)'*eye(64)*(sigma_n)^2*h(end:-1:1) ;
@@ -54,8 +59,9 @@ sigma2_y = ypik'*ypik/length(ypik) ;
 sigma2_f = fpik'*fpik/length(fpik) ;
 
 e_sigma_n = sqrt( (sigma2_y - sigma2_f/(h'*h)/N_filt)/(1-1/N_filt)) ;
+%e_sigma_n = sqrt( (sigma2_y - sigma2_f/(h'*h)/N_filt)) ;
 
 %fprintf('Theoretical sigma_n=%f\n',sigma_n) ;
-fprintf('%d peaks analyzed, sigma_n=%f, e_sigma_n=%f\n',length(fpik),sigma_n,e_sigma_n)
+fprintf('%d peaks analyzed, sigma_n=%f, e_sigma_n=%f err = %f\n',length(fpik),sigma_n,e_sigma_n, abs(sigma_n - e_sigma_n))
 str = sprintf('%d peaks analyzed, \\sigma_{n,t}=%f, \\sigma_n=%f\n',length(fpik),sigma_n,e_sigma_n) ;
 title(str,'FontSize',14)
