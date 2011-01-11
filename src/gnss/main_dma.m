9a60c05f547dc1877f32747f90e561ce281194b3
Alex Nikiforov <nikiforov.pub@gmail.com>
2011-01-12 01:38:29 +0300

    dma: some research

--- src/gnss/main_dma.m
+++ src/gnss/main_dma.m
@@ -3,27 +3,16 @@ PRN = 1:32;
 trace_me = 0;
 DumpSize = 16368*10 ;
 
-model = 0;				% is it model
+model = 1;				% is it the model?
 
 % ========= generate =======================
 if model
-   x_ca16 = ca_get(1, 0) ;
-   x_ca16 = [x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;
-       x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;
-       x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;
-       x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;
-       x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16;x_ca16] ;
-   x = exp(2*j*pi*4092000/16368000*(0:length(x_ca16)-1)).' ;
-
-    delta = 199 ;
-    x = cos(2*pi*(4092000 + delta)/16368000*(0:length(x_ca16)-1)).' ;
-    %bit_shift = round(abs(rand(1)*(length(x)-1))) ;
-    %x(bit_shift:end)=x(bit_shift:end) * (-1) ;
-    %x(length(x)/2+1000:end)=x(length(x)/2+1000:end) * (-1) ;
-    x = x .* x_ca16 ;
-    x = x(101:end);
-    x=x+randn(size(x))*2 ;
-    fprintf('Generated\n');
+	x = signal_generate(	1,	\  %PRN
+					50,	\  % freq delta in Hz
+					199,	\  % CA phase
+					0.1,	\  % noise sigma
+					DumpSize);
+	fprintf('Generated\n');
 else
 	x = readdump_bin_2bsm('./data/flush.bin', DumpSize);
 	fprintf('Real\n');
