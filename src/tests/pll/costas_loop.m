46e25d0e36070867c5e321b2f1cf8dca092c948e
Alex Nikiforov <nikiforov.pub@gmail.com>
2011-04-02 23:59:08 +0400

    pll: work on costas

--- src/tests/pll/costas_loop.m
+++ src/tests/pll/costas_loop.m
@@ -1,30 +1,68 @@
 % POC Costas loop
+% (c) Alex Nikiforov nikiforov.alex@rf-lab.org
+% License GPL v3
 
 clc; clear all;
 
 N = 16368;			% samples in 1 ms
 fd= 16.368e6;		% 16.368 MHz
 fs = 4.092e6;		% 4.092MHz
-delta  = 10;			
-fs = fs + delta;
 nco_freq = fs;		% NCO freq
+delta  = 10;			
 
 % local signal
-sig = cos(2 * pi * (fs/fd)*(0:N-1)).';
+sig = exp(2 * pi * ( (fs + delta)/fd)*(0:N-1)).';
 
-% 1 stage - downconvert
-nco_sig = exp(2 * pi * (nco_freq/fd)*(0:N-1)).';
-lo_I = sig .* real(nco_sig) ;
-lo_Q = sig .* imag(nco_sig) ;
-
-% 2 stage - filtering
 % make filter
-Fnyq = fs/2;		% nyquist freq (fs/2) Hz
-Fc=100; 		% cut-off freq Hz
-[b,a]=butter(2, Fc/Fnyq);
+%Fnyq = fd/2;					% nyquist freq (fd/2) Hz
+%Fc=100; 					% cut-off freq Hz
+%[b,a]=butter(2, Fc/Fnyq);
 %[h,t] = impz(b, a);
 %plot(t,h)
 %return;
+
+% get from filter
+a = [1; -1.99995; 0.99995];
+b =  [3.6838e-10; 7.3676e-10; 3.6838e-10];
+
+% vectors for loop
+x_I = zeros(length(a), 1);
+y_I = zeros(length(b), 1);
+x_Q = zeros(length(a), 1);
+y_Q = zeros(length(b), 1);
+
+% Loop cycle
+for k=3:10
+	% stage 1 - downconvertion
+	x_I(1)   = sig(k) .* 2 * cos(2 * pi * (nco_freq/fd) * k).' ;
+	x_Q(1) = sig(k) .* 2 * sin(2 * pi * (nco_freq/fd) * k).';
+
+	% stage 2 - LPF
+	% y(n) = b1*x(n) + b2*x(n-1) + b3*x(n-2) - a2*y(n-1) - a3*y(n-2)
+	y_I(1) = b(1)*x_I(1) + b(2)*x_I(2) + b(3)*x_I(2) - a(2)*y_I(2) - a(3)*y_I(3);
+	y_Q(1) = b(1)*x_Q(1) + b(2)*x_Q(2) + b(3)*x_Q(2) - a(2)*y_Q(2) - a(3)*y_Q(3);
+
+	% stage 3 - descriminator
+	phi = atan(y_Q(1) / y_I(1));
+	%phi = y_Q(1)* y_I(1);
+	
+	nco_freq = nco_freq + phi;
+	
+	% trace
+	fprintf('y_I = %2.5f y_Q = %2.5f phi = %2.5f\n', y_I(1),  y_Q(1), phi);
+	
+	% correct x and y vectors
+	x_I(3) = x_I(2);
+	x_I(2) = x_I(1);
+	y_I(3) = y_I(2);
+	y_I(2) = y_I(1);
+
+end
+
+return
+
+
+
 
 % stage 3
 
@@ -32,7 +70,7 @@ Fc=100; 		% cut-off freq Hz
 	lo_fI = filter(b, a, lo_I);
 	lo_fQ = filter(b, a, lo_Q);
 	
-	phi = atan(sum(lo_fQ) / sum(lo_fI))
+
 %end
 
 
