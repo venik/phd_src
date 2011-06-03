a97afa60aff536167373d8a1fd94247c2cc8e940
Alex Nikiforov <a.nikiforov@samsung.com>
2011-06-03 14:51:37 +0400

    tests: snr

--- src/tests/snr/snr_estimation.m
+++ src/tests/snr/snr_estimation.m
@@ -2,11 +2,9 @@
 
 addpath('../../gnss/');
 
-
-
 PRN = 19 ;
 N=16368;
-sigma = 0;
+sigma = 15;
 delta = 5000;
 
 ca_base = ca_get(PRN, 0) ;
@@ -28,30 +26,30 @@ corr_array = ifft(S .* conj(NS) );
 corr_array = corr_array .* conj(corr_array);
 
 % calculate SNR
-max_val = max(corr_array);
+[max_val, ca_phase] = max(corr_array);
 mean_val = mean(corr_array);
 std_val = std(corr_array);
 
 snr = 10*log10( (max_val - mean_val) / std_val );
 
-fprintf('\n Real sigma = %d\n', sigma);
+fprintf('\n Real sigma = %.2f dB sigma = %d\n', 10*log10(1/sigma^2), sigma);
 
-fprintf('max_val = %.2f in dB = %.2f\n', max_val, 10*log10(max_val));
+fprintf('max_val = %.2f in dB = %.2f phase:%03d\n', max_val, 10*log10(max_val),ca_phase);
 fprintf('mean_val = %.2f in dB = %.2f\n', mean_val, 10*log10(mean_val));
 fprintf('std_val = %.2f in dB = %.2f\n', std_val, 10*log10(std_val));
 fprintf('Result = %.2f\n', snr);
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%
-fprintf('\nWithout noise\n');
-fprintf('max_val = 66977856.00 in dB = 78.26\n');
-fprintf('mean_val = 47964.81 in dB = 46.81\n');
-fprintf('std_val = 949449.42 in dB = 59.77\n');
-fprintf('Result = 18.48\n');
+%%fprintf('\nWithout noise\n');
+%fprintf('max_val = 66977856.00 in dB = 78.26\n');
+%fprintf('mean_val = 47964.81 in dB = 46.81\n');
+%fprintf('std_val = 949449.42 in dB = 59.77\n');
+%fprintf('Result = 18.48\n');
 %%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-plot(corr_array);
+%plot(corr_array);
 
 %plot(noised_signal(1:100));
 
