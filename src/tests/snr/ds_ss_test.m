b11376ed85d85e58d36b133a1880f79d65da3b97
Alex Nikiforov <a.nikiforov@samsung.com>
2011-06-06 15:37:09 +0400

    test: some test

--- src/tests/snr/ds_ss_test.m
+++ src/tests/snr/ds_ss_test.m
@@ -3,13 +3,21 @@
 addpath('../../gnss/');
 
 PRN = 19 ;
-sigma =  15;
 delta = 500;
 
+SNR = 1;
+%sigma =  2.5;
+sigma = sqrt(10^(-SNR/10));
+
+
 ca_base = ca_generate_bits(PRN, 0);
+ca_another = ca_generate_bits(1, 0);
+
 ca_local = [ca_base] ;
 
 signal = ca_local;
+%signal_lo = signal;
+signal_lo = ca_another;
 
 noised_signal = [signal; signal];
 noised_signal = noised_signal + sigma * randn(length(noised_signal), 1);
@@ -17,7 +25,7 @@ noised_signal = noised_signal + sigma * randn(length(noised_signal), 1);
 noised_signal = noised_signal(delta : length(ca_local) + delta - 1) ;
 
 % fft convolution
-S = fft(signal);
+S = fft(signal_lo);
 NS = fft(noised_signal);
 corr_array = ifft(S .* conj(NS) );
 
@@ -35,13 +43,13 @@ mean_val = mean(corr_array);
 %  snr magic
 snr = 10*log10( (max_val - mean_val)^2 / std_val^2 );
 
-fprintf('\n Real sigma = %.2f dB sigma = %d\n', 10*log10(1/sigma^2), sigma);
+fprintf('\n Real SNR = %.2f dB sigma = %d\n', 10*log10(1/sigma^2), sigma);
 
 fprintf('max_val = %.2f in dB = %.2f phase:%03d\n', max_val, 10*log10(max_val),ca_phase);
 fprintf('mean_val = %.2f in dB = %.2f\n', mean_val, 10*log10(mean_val));
 fprintf('std_val = %.2f in dB = %.2f\n', std_val, 10*log10(std_val));
 fprintf('Result = %.2f dB\n', snr);
 
-%plot(corr_array);
+plot(corr_array);
 
 rmpath('../../gnss/');
