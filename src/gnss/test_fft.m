1cab9a32f2b92dd6a498a1e043a7ef11fa368fad
Alex Nikiforov <nikiforov.pub@gmail.com>
2011-07-17 22:59:56 +0400

    test: add file store

--- src/gnss/test_fft.m
+++ src/gnss/test_fft.m
@@ -1,6 +1,6 @@
 clc; clear all; clf;
 
-DumpSize = 16368*6 ;
+DumpSize = 16368*1 ;
 N = 16368 ;
 fs = 4.092e6-5e3 : 1e3 : 4.092e6+5e3 ;		% sampling rate 4.092 MHz
 ts = 1/16.368e6 ;
@@ -8,12 +8,18 @@ ts = 1/16.368e6 ;
 model = 1;				% is it the model?
 acx_res = zeros(4) ;		% [acx, ca_phase, freq, detected state]
 
-sigma = [0.01, 0.1, 1, 1.5, 2];
+%sigma = [0.01, 0.1, 1, 1.5, 2];
+sigma = 0.01:0.1:2;
+snr = 10 * log10(1./sigma);
 
 detected = zeros(length(sigma), 1);
+probability = zeros(length(sigma), 1);
+
+fd = fopen('test_fft.res', 'w+', 'ieee-le');
 
 for k=1:length(sigma)
-for h=1:10
+%for h=1:10
+for h=1:1000
 	x = signal_generate(	1,	\  %PRN
 					1,	\  % freq delta in Hz
 					1,	\  % CA phase
@@ -28,17 +34,23 @@ for h=1:10
 			detected(k) = detected(k) + 1;
 	end;
 end;	% for h=1
+
+	% dump it
+	probability(k) = detected(k) / h * 100;
+	str = sprintf("%1.2f     %3.2f\n", snr(k), probability(k));
+	fwrite(fd, str);
 end	%for k=1
 
 % results
-snr = 10 * log10(1./sigma);
 probability = detected / h * 100;
 for k=1:length(sigma)
 	%fprintf('sigma:%f (SNR = %f)\tdetected = %d from i = %d\n', sigma(k), snr(k), detected(k), i);
-	fprintf('SNR = %f \t probabilit = %3.2f\n', SNR(k), probability(k));
+	fprintf('SNR = %f \t probabilit = %3.2f\n', snr(k), probability(k));
 end	%for k=1
 
+fclose(fd);
+
 plot(snr, probability),
-	xlabel('SNR'),
+	xlabel('SNR, dB'),
 	ylabel('Probalility, %')
 	title('Probability of detection');
