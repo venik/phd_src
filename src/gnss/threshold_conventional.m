df8ae97dae61f55c2af19eeb63397c316f4aa590
Alex Nikiforov <nikiforov.pub@gmail.com>
2011-07-18 23:50:37 +0400

    threshold: add stub

---
+++ src/gnss/threshold_conventional.m
@@ -0,0 +1,25 @@
+%    Signal detection based on 2 peaks
+%    Copyright (C) 2010 - 2011 Alex Nikiforov  nikiforov.alex@rf-lab.org
+%
+%    This program is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+
+%    This program is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+
+%    You should have received a copy of the GNU General Public License
+%    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+%
+%    Reference: this approach used in fastgps software
+
+%    Input values:	x - data after correlation in time domain
+%			trace_me - trace flag
+%   Output values: 
+function res = threshold_conventional(x, Pfa, trace_me)
+% TODO	
+
+end	% function
