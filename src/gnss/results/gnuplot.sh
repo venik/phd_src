c96fba1dda6f8e550f16d9495d06f559741f316a
Alex Nikiforov <nikiforov.pub@gmail.com>
2011-07-18 07:54:26 +0400

    test: results: add fft 2max results and gnuplot script

---
+++ src/gnss/results/gnuplot.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/gnuplot -persist
+
+set terminal postscript eps enhanced
+set output "test_fft.ps"
+set encoding koi8r
+
+set xlabel "SNR, dB" font "Helvetica,18"
+set ylabel "Probablility, %" font "Helvetica,18"
+set style line 1 lt 1 pt 7
+
+# grid ON
+set grid xtics ytics mxtics mytics
+
+#plot "test_fft.res" using 2 title "СКО" with linespoints linestyle 1
+plot "test_fft.res" using 1:2 title "Parallel correlator" with linespoints linestyle 1
