eecb4ccd5e16b63fe6f4fb2d687c616388977753
Alex Nikiforov <nikiforov.pub@gmail.com>
2011-07-20 01:17:55 +0400

    test: snr: rscn: add rscn algo

--- src/tests/snr/rscn.m
+++ src/tests/snr/rscn.m
@@ -1,3 +1,23 @@
+%    Real signal complex noise (RSCN) noise estimator
+%    Copyright (C) 2010 Alex Nikiforov  nikiforov.alex@rf-lab.org
+%
+%    This program is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+
+%    This program is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+
+%    You should have received a copy of the GNU General Public License
+%    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+%
+% Reference:	Brad Badke "Carrier to-Noise Density and AI for INS/GPS Integration", InsideGNSS magazine, pp 20-29, september/october 2009.
+%			http://www.insidegnss.com/node/1637
+%			http://www.insidegnss.com/auto/sepoct09-gnss-sol.pdf
+
 clc; clear all; clf;
 
 addpath('../../gnss/');
