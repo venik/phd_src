30304cadd269bf4aee520c552d18c0aafff5a04d
Alex Nikiforov <nikiforov.pub@gmail.com>
2011-07-21 23:59:05 +0400

    test: rscn: work on it, add dB scale

--- src/tests/snr/rscn.m
+++ src/tests/snr/rscn.m
@@ -29,20 +29,31 @@ PRN = 1;
 DumpSize = N * 2;
 freq_delta = 0;
 ca_phase = 8000;
-sigma_low = 0.1:0.1:0.9;
-sigma_high = 1:15;
-sigma = [sigma_low, sigma_high];
+%sigma_low = 0.1:0.1:0.9;
+%sigma_high = 1:15;
+%sigma = [sigma_low, sigma_high];
+sigma = 5;
+snr = -6:6;
 estimated_sigma = zeros(length(sigma), 1);
-
-for k=1:length(sigma)
+estimated_snr = zeros(length(snr), 1);
+%for k=1:length(sigma)
+for k=1:length(snr)
 	% make signal
-	x_ca16 = ca_get(PRN, 0) ;
-	x_ca16 = repmat(x_ca16, DumpSize/N + 1, 1);
-	x = cos(2*pi*(fs + freq_delta)/fd*(0:length(x_ca16)-1)).' ;
-	x = x .* x_ca16 ;
-	x = x(ca_phase:DumpSize + ca_phase - 1);
-	wn = (sigma(k)/sqrt(2)) * (randn(DumpSize, 1) + j * randn(DumpSize, 1));
-	x = x + wn ;
+	if 0
+		x_ca16 = ca_get(PRN, 0) ;
+		x_ca16 = repmat(x_ca16, DumpSize/N + 1, 1);
+		x = cos(2*pi*(fs + freq_delta)/fd*(0:length(x_ca16)-1)).' ;
+		x = x .* x_ca16 ;
+		x = x(ca_phase:DumpSize + ca_phase - 1);
+		wn = (sigma(k)/sqrt(2)) * (randn(DumpSize, 1) + j * randn(DumpSize, 1));
+		x = x + wn ;
+		
+		fprintf('real snr = %f and %f in dB\n', 0.5/sigma^2, 10*log10(0.5/sigma^2));
+	else
+		x = signal_generate(PRN, freq_delta, ca_phase, snr(k), DumpSize, 1);
+		fprintf('real snr = %f in dB\n', snr);
+	end; %if 1
+		
 	
 	% test
 	lo_ca = ca_get(PRN, 0) ;
@@ -59,24 +70,35 @@ for k=1:length(sigma)
 	corr_res = acx .* conj(acx);
 	[max_val, est_ca_phase] = max(corr_res);
 	
-	%
+	% SNR estimation
 	for_noise = x .* lo_replica(est_ca_phase : est_ca_phase + N - 1);
 	
-	acx_real = real(for_noise);
-	Q_acc_2_real = mean(acx_real.^2);
+	%acx_real = real(for_noise);
+	%Q_acc_2_real = mean(acx_real.^2);
 	
 	acx_imag = imag(for_noise);
-	Q_acc_2_imag = mean(acx_imag.^2);
+	%Q_acc_2_imag = mean(acx_imag.^2);
+	acx_imag = acx_imag.^2;
+	Q_acc_2_imag = sum(acx_imag(1:end)) / N;
 	
-	estimated_sigma(k) = sqrt(Q_acc_2_real + Q_acc_2_imag);
+	total_power = for_noise .* conj(for_noise);
+	total_power = sum(total_power(1:end)) / N;
+	estimated_snr(k) = (total_power - 2*Q_acc_2_imag) / (2*Q_acc_2_imag);
+	fprintf('snr = %f and snr = %f in dB\n', estimated_snr(k), 10*log10(estimated_snr(k)));
+	estimated_snr(k)= 10*log(estimated_snr(k));
+
+	%estimated_sigma(k) = sqrt(Q_acc_2_real + Q_acc_2_imag);
+	%estimated_sigma(k) = sqrt(2*Q_acc_2_imag);
+	
 	%plot(corr_res);
 	%fprintf('real_sigma = %f \t estimated_sigma = %f\n', sigma, estimated_sigma);
 end	% for k
 
-plot(sigma, sigma, '-rx', sigma,estimated_sigma, '-go'),
+%plot(sigma, sigma, '-rx', sigma,estimated_sigma, '-go'),
+plot(snr, snr, '-rx', snr, estimated_snr, '-go'),
 	legend('real sigma', 'estimated sigma'),
-	xlim([0, sigma(end)]),
+	xlim([snr(1), snr(end)]),
 	grid on;
-print -djpeg 'rscn_sigma.jpg';
+%print -djpeg 'rscn_sigma.jpg';
 
 rmpath('../../gnss/');
