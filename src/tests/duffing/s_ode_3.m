ff0852320a2bc993fd7368693e2e86b3a4342f87
Alex Nikiforov <nikiforov.alex@rf-lab.org>
2012-04-23 23:11:03 +0400

    test: duffing: add some

--- src/tests/duffing/s_ode_3.m
+++ src/tests/duffing/s_ode_3.m
@@ -2,7 +2,9 @@
 % Duffing
 function s_ode_3()
 x0=[0;0];
-tspan=[0,5];
+
+tspan=[0:0.01:15];
+
 [t,x]=ode45(@eq1,tspan,x0);
 clf; plot(x(:,1),x(:,2)), grid on, hold on, comet(x(:,1),x(:,2)) ;
 %clf; plot3(x(:,1),x(:,2),t), grid on,xlabel('x_1'),ylabel('x_2'),zlabel('t'), hold on, comet3(x(:,1),x(:,2),t) ;
@@ -10,6 +12,9 @@ function f = eq1(t,x)
 gamma=1 ;
 w0 = 8 ;
 w = 8 ;
-A = 1000 ;
+A = 1.7 ;
 beta = 1000 ;
-f=[x(2);-gamma*x(2)-w0^2*x(1)-beta*x(1)^3+A*cos(w*t)] ;
+k = 1;
+%f=[x(2);-w0*k*x(2) + +w0^2*(x(1)-x(1)^3) + w0^2*A*cos(w0*t) + cos(w*t)] ;
+f=[x(2);-w0*k*x(2) + x(1)^3 - x(1)^5 + 1*cos(w0*t) + 0.2*cos(w*t)] ;
+%f=[x(2);-0.5*x(2) + x(1)^3 - x(1)^5 + 0.78*cos(w0*t)] ;
