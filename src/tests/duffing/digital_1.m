2b1c148644f4e56dfc88d95821424ebce2399003
Alex Nikiforov <nikiforov.pub@gmail.com>
2012-07-12 02:16:59 +0400

    test: duffing: add volterra

--- src/tests/duffing/digital_1.m
+++ src/tests/duffing/digital_1.m
@@ -1,71 +1,177 @@
+% Copyright (C)
+%   2012 Alex Nikiforov  nikiforov.alex@rf-lab.org
+%	2012 Alexey Melnikov  melnikov.alexey@rf-lab.org
+%
 % http://www.math.tamu.edu/REU/comp/matode.pdf
 % Duffing
 % Volterra series on Duffing
 
-function digita_l()
+function runge_kutta()
+clc;
 
-step = 0.0002 ;
-x0=[0;0];
-tspan=0:step:1;
+global gamma_x;
+global gamma;
+global delta_t;
+global w;
+global w_x;
+global sigma;
+global k;
 
-iter = 1 ;
-varss = zeros(iter, 1) ;
-for i=1:iter
-    [t,x]=ode45(@eq1,tspan,x0, 1) ;
+% Duffing constants
+gamma = 0.4;
+gamma_x = 0.385 ;
+w = 1  ;
+k = 0.5;
 
-    %h= firls(30, [ 0  40/(5000*2)   60/(5000*2)  1 ],[ 0 0 1 1  ]) ;
-    %fvtool(h,1) ;
-    %y = filter(h,1,x(:, 1)) ;
-    %y=x(:, 1);
+% presence signal
+delta_t = 0.01;
+t = 0:delta_t:500;
+
+% convert to SNR 10*log10(0.5/sigma)
+sigma = 0 ;
+noise = sigma * randn(length(t), 1) ;
+
+% Incoming signal
+w_x = w ;
+
+% [x; x']
+x = zeros(length(t) + 1, 2) ;
+
+% Cauchy coditions
+x(1, :) = [1; 1];
+x(1, 2) = 1;
+
+for duff = 1:length(t)
+    x(duff + 1, :) = step(t(duff), x(duff, :), noise(duff));
+end % for
+
+fprintf('Variance %f\n', var(x(100:end,1)));
+
+% plot
+    clf; figure(1), plot(x(:,1),x(:,2)),
+        xlabel('x'), ylabel('y'),
+        grid on; %, hold on, comet(x(:,1),x(:,2));
+
+    [spectrum, f] = pwelch(x(:, 1)); spectrum = spectrum .* conj(spectrum);
+    [a,b] = max(spectrum);
+    fprintf(' max %f, position %d\n', a, b);
+    figure(2), plot(f(1:500), spectrum(1:500)), grid on, title(sprintf('Max %f, position %d SNR:%2.2f', a,b, 10*log10(0.5/sigma)));
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%    
+% absence signal
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+gamma_x = 0 ;
+w_x = 1 ;
+
+% convert to SNR 10*log10(0.5/sigma)
+sigma = 2 ;
+noise = sigma * randn(length(t), 1) ;
+
+% [x; x']
+x_noise = zeros(length(t) + 1, 2) ;
+
+% Cauchy coditions
+x_noise(1, :) = [1; 1];
+x_noise(1, 2) = 1;
+
+for duff = 1:length(t)
+    x_noise(duff + 1, :) = step(t(duff), x_noise(duff, :), noise(duff));
+end % for
+
+% plot
+    clf; figure(3), plot(x_noise(:,1),x_noise(:,2)),
+        xlabel('x'), ylabel('y'),
+        grid on; %, hold on, comet(x(:,1),x(:,2));
+
+    [spectrum, f] = pwelch(x_noise(:, 1)); spectrum = spectrum .* conj(spectrum);
+    [a,b] = max(spectrum);
+    fprintf(' max %f, position %d\n', a, b);
+    figure(4), plot(f(1:500), spectrum(1:500)), grid on, title(sprintf('Max %f, position %d SNR:%2.2f', a,b, 10*log10(0.5/sigma)));
+
     
-    %spectrum = pwelch(y); spectrum = abs(spectrum) ;
-    [spectrum, f] = pwelch(x,[],[],[],1/step); spectrum = abs(spectrum) ;
-    %spectrum = fft(x); spectrum = abs(spectrum) ;    
-    %varss(i) = var(spectrum) ;
-    [energy, freq] = max(spectrum) ;
-    fprintf('%03d: freq:%02d: energy:%5.2f\n', i, freq, energy) ;
-end ;
-
-%if iter>1
-%    hist(varss, 100);
-%end;
-
-%if iter==1
-%clf; figure(1),
-%    figure(1), plot(x(:,1),x(:,2)), grid on, title('Phase plane'); %, hold on, comet(x(:,1),x(:,2)) ;
-%    figure(2), plot(f, spectrum), grid on, title('Spectrum') ;
-%end
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%% digital part %%%%%%%%%%%%%%%%%%%%%%%%%%%
-num_eq = 1;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+num_eq = length(t) - 1;
 
+% [1  x_k  x_k-1  x_k^2  x_k*x_k-1  x_k-1^2]
 length_of_vec = 6 ;
 
-mtx_coef = ones(num_eq, length_of_vec) ; 
+mtx_coef = ones(2 * num_eq, length_of_vec) ;
 
+% signal
 for k = 1:num_eq
-    mtx_coef(k, 2:length_of_vec) = x(1:length_of_vec-1)
-    mtx_coef(k, 4) =  mtx_coef(k, 4)^2 ;
-    mtx_coef(k, 6) =  mtx_coef(k, 6)^2 ;
-    %mtx_coef(k, )
+    mtx_coef(k, 2:3) = [cos(w*t(k+1)), cos(w*t(k))] ;   x(k+1:-1:k) ;
+    % adjust coef
+    mtx_coef(k, 4) = mtx_coef(k, 2)^2 ;
+    mtx_coef(k, 5) = mtx_coef(k, 2) * mtx_coef(k, 3) ;
+    mtx_coef(k, 6) = mtx_coef(k, 3)^2 ;
 end ;  % for k
 
-mtx_coef
+% noise
+for k = num_eq+1:2*num_eq
+    mtx_coef(k, 2:3) = [noise(k+1-num_eq), noise(k-num_eq)] ;          %x(k+1:-1:k) ;
+    % adjust coef
+    mtx_coef(k, 4) = mtx_coef(k, 2)^2 ;
+    mtx_coef(k, 5) = mtx_coef(k, 2) * mtx_coef(k, 3) ;
+    mtx_coef(k, 6) = mtx_coef(k, 3)^2 ;
+end ;  % for k
 
-function f = eq1(t,x)
+mtx_coef(1:3,:)
 
-global noise
+out_vec = [x(1:end-2, 1); x_noise(1:end-2, 1)] ;
 
-gamma = 0.826 ;
-w = 100 * 2 * pi ;
-k = 0.5; 
+%size([x(1:end-2, 1); x_noise(1:end-2, 1)])
+%size(mtx_coef)
 
-gamma_x = 0;
-w_x = w ;
-sigma = 0;
-noise = sigma * randn(1);
+h = pinv(mtx_coef) * out_vec
+    
+% Incoming parameters:
+%   t - current time
+%   x(1) - x
+%   x(2) - x'
+% Return parameters:
+%   y(1) - y
+%   y(2) - y'
+function y = step(t, x, noise)
 
-%input = gamma_x * cos(w_x*t + phase) + noise;
-input = 0;
+global gamma_x;
+global w_x;
+global gamma;
+global w;
+global k;
+global delta_t;
 
-f=[x(2) * w ; w*(-k*x(2) + x(1) - x(1)^3 + gamma*cos(w*t) + input)] ;
+% calculate Runge-Kutta step
+k1 =    gamma_x * cos(w_x*t) + ...
+        gamma * cos(w*t) + ...
+        noise + ...
+        x(1)^3 - x(1)^5 - ...
+        k*x(2) ;
+    
+x_tmp = x(1) + x(2) * delta_t / 2 ;
+x_der = x(2) + k1 / 2 ;
+k2 =    gamma_x * cos(w_x * (t + delta_t / 2)) + ...
+        gamma * cos(w * (t + delta_t / 2)) + ...
+        noise + ...
+        x_tmp^3 - x_tmp^5 - ...
+        k*x_der ;
+    
+x_tmp = x(1) + x(2) * delta_t / 2 + k1/4 * delta_t ;
+x_der = x(2) + k2 / 2 ;
+k3 =    gamma_x * cos(w_x * (t + delta_t / 2)) + ...
+        gamma * cos(w * (t + delta_t / 2)) + ...
+        noise + ...
+        x_tmp^3 - x_tmp^5 - ...
+        k*x_der;
+        
+x_tmp = x(1) + x(2) * delta_t + k2/2 * delta_t;
+x_der = x(2) + k3 ;
+k4 =    gamma_x * cos(w_x * (t + delta_t)) + ...
+        gamma * cos(w * (t + delta_t)) + ...
+        noise + ...
+        x_tmp^3 - x_tmp^5 - ...
+        k*x_der ;
+
+y(1) = x(1) + delta_t * (x(2) + delta_t/6 * (k1 + k2 + k3)) ;
+y(2) = x(2) + delta_t/6 * (k2 + 2*k2 + 2*k3 + k4) ;
