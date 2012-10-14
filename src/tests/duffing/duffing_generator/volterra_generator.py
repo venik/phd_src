ffe2f2bfd098f2930060acd32529ae50f57cd6fb
Alex Nikiforov <nikiforov.pub@gmail.com>
2012-10-15 01:40:26 +0400

    test: duffing: make it more flexible

--- src/tests/duffing/duffing_generator/volterra_generator.py
+++ src/tests/duffing/duffing_generator/volterra_generator.py
@@ -38,25 +38,11 @@ volt_cycle_st = '\nlen = size(f,2) ;\n'		\
 		'X = zeros(len-%d, 18) ;\n'	\
 		'for n=%d:len'
 
-volt_cycle_body='  X(n-%d,%d) = 1 ;\n'		\
-		'  X(n-%d,%d) = f(n) ;\n'	\
-		'  X(n-%d,%d) = f(n)*f(n) ;\n'	\
-		'  X(n-%d,%d) = f(n)*f(n)*f(n) ;\n' \
-		'  X(n-%d,%d) = f(n-1) ;\n'	\
-		'  X(n-%d,%d) = f(n-1)*f(n-1) ;\n'  \
-		'  X(n-%d,%d) = f(n-1)*f(n-1)*f(n-1) ;'
+volt_cycle_body='  X(n-%d,%d) = 1 ;\n'
 
-volt_cycle_body1='\n  X(n-%d,%d) = f(n)*f(n-1) ;\n'		\
-		 '  X(n-%d,%d) = f(n)*f(n-1)*f(n-1) ;\n'		\
-		 '  X(n-%d,%d) = f(n)*f(n)*f(n-1) ;'
-
-volt_cycle_body2='\n  X(n-%d,%d) = f(n-2)*f(n)*f(n) ;\n'	\
-                 '  X(n-%d,%d) = f(n-2)*f(n-1)*f(n) ;\n'	\
-                 '  X(n-%d,%d) = f(n-2)*f(n-1)*f(n-1) ;\n'	\
-		 '  X(n-%d,%d) = f(n-2)*f(n-2)*f(n) ;\n'	\
-		 '  X(n-%d,%d) = f(n-2)*f(n-2)*f(n-1) ;\n\n'
-
-volt_cycle_body3='  X(n-%d,%d) = f(n-%d) ;\n'
+volt_cycle_body1='  X(n-%d,%d) = f(n-%d) ;\n'
+volt_cycle_body2='  X(n-%d,%d) = f(n-%d)*f(n-%d) ;\n'
+volt_cycle_body3='  X(n-%d,%d) = f(n-%d)*f(n-%d)*f(n-%d) ;\n'
 
 volt_cycle_end = 'end\n\n'		\
 		 'size(pinv(X))\n'	\
@@ -74,16 +60,9 @@ volt_cycle2 = 'r = zeros(size(y)) ;\n'	\
 volt_cycle2_end = 'end\n' \
 		  'fprintf(\'Variance volterra %f\\n\', var(r));\n'
 
-volt_cycle2_body1 = '  r(n) = h(1) + h(2)*f(n) + h(3)*f(n)*f(n) + h(4)*f(n)*f(n)*f(n) + '	\
-		    'h(5)*f(n-1) + h(6)*f(n-1)*f(n-1) + h(7)*f(n-1)*f(n-1)*f(n-1) + ...\n'	\
-        	    '  h(8)*f(n)*f(n-1) + h(9)*f(n)*f(n-1)*f(n-1) + h(10)*f(n)*f(n)*f(n-1) + ...\n'\
-                    '  h(11)*f(n-2)*f(n)*f(n) + h(12)*f(n-2)*f(n-1)*f(n) + '			\
-		    'h(13)*f(n-2)*f(n-1)*f(n-1) + h(14)*f(n-2)*f(n-2)*f(n) + '			\
-		    'h(15)*f(n-2)*f(n-2)*f(n-1) + ...\n'
-
-volt_cycle2_body1_1 = '  h(%d)*f(n-%d) + '
-volt_cycle2_body1_1_last = '  h(%d)*f(n-%d) ;\n'
-
+volt_cycle2_body1 = 'h(%d)*f(n-%d) + '
+volt_cycle2_body2 = 'h(%d)*f(n-%d)*f(n-%d) + '
+volt_cycle2_body3 = 'h(%d)*f(n-%d)*f(n-%d)*f(n-%d) + '
 
 def put_line(fd, data, new_line = True):
 	if new_line:
@@ -110,32 +89,37 @@ put_line(fd_test_m, func_noise)
 put_line(fd_test_m, our_func % ('noise'), False)
 
 # volterra
-deep = 500
+deep = 3 
 eq_num = 1
 put_line(fd_m, volt_cycle_st % (deep-1, deep))
-put_line(fd_m, volt_cycle_body % (	deep-1, eq_num,		\
-					deep-1, eq_num+1, 	\
-					deep-1, eq_num+2,	\
-					deep-1, eq_num+3,	\
-					deep-1, eq_num+4,	\
-					deep-1, eq_num+5,	\
-					deep-1, eq_num+6))
-eq_num += 7;
-put_line(fd_m, volt_cycle_body1 % (	deep-1, eq_num,		\
-					deep-1, eq_num+1, 	\
-					deep-1, eq_num+2))
-eq_num += 3;
+put_line(fd_m, volt_cycle_body % (deep-1, eq_num))
+eq_num += 1
 
-put_line(fd_m, volt_cycle_body2 % (	deep-1, eq_num,		\
-					deep-1, eq_num+1, 	\
-					deep-1, eq_num+2,	\
-					deep-1, eq_num+3,	\
-					deep-1, eq_num+4))
-eq_num += 5;
-
-for i in range(2, deep):
-	put_line(fd_m, volt_cycle_body3 % (deep-1, eq_num, i), False)	
+# 1 order
+for i in range(0, deep):
+	put_line(fd_m, volt_cycle_body1 % (deep-1, eq_num, i), False)	
 	eq_num += 1
+
+put_line(fd_m, '')
+
+# 2 order
+for i in range(0, deep):
+	for j in range(0, deep):
+		if (j >= i):
+			#print("i=%d j=%d" % (i,j))
+			put_line(fd_m, volt_cycle_body2 % (deep-1, eq_num, i, j), False)
+			eq_num += 1
+
+put_line(fd_m, '')
+
+# 3 order
+for i in range(0, deep):
+	for j in range(0, deep):
+		for k in range(0, deep):
+			if (k >= j) and (j >= i):
+				#print("i=%d j=%d k=%d" % (i,j,k))
+				put_line(fd_m, volt_cycle_body3 % (deep-1, eq_num, i, j, k), False)
+				eq_num += 1
 
 put_line(fd_m, volt_cycle_end % (deep, deep), False)
 
@@ -148,31 +132,57 @@ put_line(fd_test_m, load_mat, False)
 
 # cycle for plotting
 put_line(fd_m, volt_cycle2 % (deep))
-put_line(fd_m, volt_cycle2_body1, False)
-
 put_line(fd_test_m, volt_cycle2 % (deep))
-put_line(fd_test_m, volt_cycle2_body1, False)
-
-# cycle for calculation of output of a volterra series
-eq_num = 16
-line_feed_num = 1
-for i in range(2, deep-1):
-	put_line(fd_m, volt_cycle2_body1_1 % (eq_num, i), False)	
-	put_line(fd_test_m, volt_cycle2_body1_1 % (eq_num, i), False)	
-	eq_num += 1
-	line_feed_num += 1
-
-	if line_feed_num == 5:
-		put_line(fd_m, " ...\n", False)
-		put_line(fd_test_m, " ...\n", False)
-		line_feed_num = 1
 
 # end of cycle for calculation of the volterra series
-put_line(fd_m, volt_cycle2_body1_1_last % (eq_num, i + 1), False)
-put_line(fd_m, volt_cycle2_end, False)
+eq_num = 2
+put_line(fd_m, '  r(n) = h(1) + ', False)
+put_line(fd_test_m, '  r(n) = h(1) + ', False)
+# 1 order
+for i in range(0, deep):
+	put_line(fd_m, volt_cycle2_body1 % (eq_num, i), False)	
+	put_line(fd_test_m, volt_cycle2_body1 % (eq_num, i), False)	
+	eq_num += 1
+	if (eq_num % 5) == 0:
+		put_line(fd_m, " ...\n", False)
+		put_line(fd_test_m, " ...\n", False)
 
-put_line(fd_test_m, volt_cycle2_body1_1_last % (eq_num, i + 1), False)
-put_line(fd_test_m, volt_cycle2_end, False)
+# 2 order
+put_line(fd_m, "  ...\n", False)
+put_line(fd_m, '  ', False)
+put_line(fd_test_m, "  ...\n", False)
+put_line(fd_test_m, '  ', False)
+for i in range(0, deep):
+	for j in range(0, deep):
+		if (j >= i):
+			put_line(fd_m, volt_cycle2_body2 % (eq_num, i, j), False)	
+			put_line(fd_test_m, volt_cycle2_body2 % (eq_num, i, j), False)	
+			eq_num += 1
+			if (eq_num % 5) == 0:
+				put_line(fd_m, " ...\n", False)
+				put_line(fd_m, '  ', False)
+				put_line(fd_test_m, " ...\n", False)
+				put_line(fd_test_m, '  ', False)
+
+# 3 order
+for i in range(0,3):
+	for j in range(0,3):
+		for k in range(0,3):
+			if (k >= j) and (j >= i):
+				put_line(fd_m, volt_cycle2_body3 % (eq_num, i, j, k), False)	
+				put_line(fd_test_m, volt_cycle2_body3 % (eq_num, i, j, k), False)	
+				eq_num += 1
+				if (eq_num % 5) == 0:
+					put_line(fd_m, " ...\n", False)
+					put_line(fd_m, '  ', False)
+					put_line(fd_test_m, " ...\n", False)
+					put_line(fd_test_m, '  ', False)
+
+put_line(fd_m, '0 ;', False)
+put_line(fd_m, volt_cycle2_end)
+
+put_line(fd_test_m, '0 ;', False)
+put_line(fd_test_m, volt_cycle2_end)
 
 # plot output from volterra and duffing 
 put_line(fd_m, figure)
