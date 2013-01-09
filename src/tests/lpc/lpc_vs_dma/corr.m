8a6c901506d49a12cd4a7c54a0f5e68205ccf634
Alex Nikiforov <nikiforov.pub@gmail.com>
2013-01-10 00:55:48 +0400

    tests: lpc: fix some

--- src/tests/lpc/lpc_vs_dma/corr.m
+++ src/tests/lpc/lpc_vs_dma/corr.m
@@ -3,8 +3,9 @@ clc, clear all ;
 N = 16368 ;
 fd = 16368 ;
 
-snr_db = 30 ;
-acx = zeros(numel(snr_db), 1) ;
+% reference -16:5
+snr_db = -17:0.5:6 ;
+tries = 100 ;
 
 % hardcoded sat 1
 code = get_ca_code16(1023, 1) ;
@@ -13,16 +14,37 @@ code = get_ca_code16(1023, 1) ;
 c = cos(2*pi*3800/fd*(0:N-1))' ;
 CA = fft(code(1:N) .* c);
 
+% we have only cos so devide to 2, and devide 2 bcoz half E
+thr = N / 2 / 2 ;
+
+succ = zeros(numel(snr_db), 1) ;
+
 for kk=1:numel(snr_db)
-    [y,sats, delays] = if_signal_model(snr_db(kk)) ;
-    Y = fft(y(1:N));
+    fprintf('snr %02.2f:\n', snr_db(kk));
+    
+    for kkk=1:tries
+        [y,sats, delays] = if_signal_model(snr_db(kk)) ;
+        Y = fft(y(1:N));
 
-    % correlate
-    acx = ifft(CA .* conj(Y));
-    %acx = acx .* conj(acx);
-    acx = abs(acx) ;
+        % correlate
+        acx = ifft(CA .* conj(Y));
+        %acx = acx .* conj(acx);
+        acx = abs(acx) ;
 
-    max(acx)
+        [energy, phase] = max(acx) ;
+        if energy >= thr && phase == 1024
+            succ(kk) = succ(kk) + 1 ;
+            % fprintf('\t\t %02d success %02d\n', kkk, succ(kk)) ;
+        end % if
+        
+        %max(acx)
+        %plot(acx)
+    end % kkk
+    
+    succ(kk) = succ(kk) / tries ;
 
-    plot(acx)
-end
+end % kk
+
+save('result_corr.mat', 'snr_db', 'succ') ;
+
+plot(snr_db, succ) ;
--- src/tests/lpc/lpc_vs_dma/dma.m
+++ src/tests/lpc/lpc_vs_dma/dma.m
@@ -1,6 +1,8 @@
 clc, clear all ;
 
-snr_db = 10;
+% reference -16:5
+snr_db = -17:0.5:6 ;
+tries = 50 ;
 
 % hardcoded sat 1
 code = get_ca_code16(1023, 1) ;
@@ -10,37 +12,57 @@ N = 16368 ;
 iteration = 1 ;
 signal = zeros(N,1);
 
+% we have only cos so devide to 2, and devide 2 bcoz half E
+thr = N / 2 / 2 ;
+
 % tau = 1/freq / 1/fs
 tau = 43 ;
 
+succ = zeros(numel(snr_db), 1) ;
+
 for kk=1:numel(snr_db)
-    [y,sats, delays] = if_signal_model(snr_db(kk)) ;
+    fprintf('snr %02.2f:\n', snr_db(kk));
     
-    for k=1:iteration
-        signal(1:N) = signal(1:N) + y((k-1)*N + 1: k*N) .* conj(y((k-1)*N + 1 + tau: k*N + tau));
-    end % for()
+    for kkk=1:tries
+        [y,sats, delays] = if_signal_model(snr_db(kk)) ;
 
-    % get new code
-    ca_new_code = signal ./ iteration;
-    CA_NEW_CODE = fft(ca_new_code);
+        for k=1:iteration
+            signal(1:N) = signal(1:N) + y((k-1)*N + 1: k*N) .* conj(y((k-1)*N + 1 + tau: k*N + tau));
+        end % for k
 
-    % generate local replica of the new code
-    ca_new_tmp = code(1:N) .* code(1+tau : N+tau);
-    CA_NEW_TMP = fft(ca_new_tmp);
+        % get new code
+        ca_new_code = signal ./ iteration;
+        CA_NEW_CODE = fft(ca_new_code);
 
-    % correlate
-    acx = ifft(CA_NEW_TMP .* conj(CA_NEW_CODE));
-    %acx = acx .* conj(acx);
-    acx = abs(acx) ;
+        % generate local replica of the new code
+        ca_new_tmp = code(1:N) .* code(1+tau : N+tau);
+        CA_NEW_TMP = fft(ca_new_tmp);
 
-    % tt = signal(1:N) .* code(1024 : 16368 + 1023) ;
-    % [b a] = butter(5, 0.0001) ;
-    % signal = filter(b,a,signal) ;
-    % plot(abs(fft(tt)))
+        % correlate
+        acx = ifft(CA_NEW_TMP .* conj(CA_NEW_CODE));
+        %acx = acx .* conj(acx);
+        acx = abs(acx) ;
 
-    max(acx)
-    plot(acx)
+        % tt = signal(1:N) .* code(1024 : 16368 + 1023) ;
+        % [b a] = butter(5, 0.0001) ;
+        % signal = filter(b,a,signal) ;
+        % plot(abs(fft(tt)))
+        
+        [energy, phase] = max(acx) ;
+        if energy >= thr && phase == 1024
+            succ(kk) = succ(kk) + 1 ;
+            % fprintf('\t\t %02d success %02d\n', kkk, succ(kk)) ;
+        end % if
+        
+        %max(acx)
+        %plot(acx)
+    end % kkk
     
-end
+    succ(kk) = succ(kk) / tries ;
+    
+end % kk
 
+save('result_dma.mat', 'snr_db', 'succ') ;
+
+plot(snr_db, succ, '-gx') ;
 
