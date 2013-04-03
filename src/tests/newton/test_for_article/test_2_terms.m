842a24a3067503287cd51cc3dcf0b3f40273756e
Alex Nikiforov <nikiforov.alex@rf-lab.org>
2013-04-04 02:16:41 +0400

    test: newton: 3 terms and 2 terms compare

--- src/tests/newton/test_for_article/test_2_terms.m
+++ src/tests/newton/test_for_article/test_2_terms.m
@@ -5,7 +5,7 @@ clc, clear all ;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 n2 = -5:15 ; % !!! in SNR dB
 A = 1 ;     % sine amplitude
-times = 100 ;% number of simulation times
+times = 10 ;% number of simulation times
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 N = 16368 ;
@@ -15,6 +15,9 @@ fsig = 3000 ;
 w_delta = zeros(numel(n2), 1) ;
 A_delta = zeros(numel(n2), 1) ;
 
+w_3_delta = zeros(numel(n2), 1) ;
+A_3_delta = zeros(numel(n2), 1) ;
+
 for i=1:numel(n2)
     fprintf('Stage %d dB\n', n2) ;
     for j=1:times
@@ -22,50 +25,68 @@ for i=1:numel(n2)
         signoise = 10^(-n2(i)/10)*var(x) ;
         x = x + randn(1,N)*sqrt(signoise) ;
         rxx = [x*circshift(x,0)';x*circshift(x',1);x*circshift(x',2)] ;
-        %txx = [cos(2*pi*fsig/16368*0);cos(2*pi*fsig/16368*1);cos(2*pi*fsig/16368*2)]*N/2 ;
-        %cxx = [txx(1)+n2*N;txx(2);txx(3)] ;
-        %[rxx, txx, cxx]
-
+        
         % Utilize Newton iterations to compute 
-        % Signal Energy, Noise Energy and Frequency
-        z = [rxx(1)/2;1] ;
+        % Signal Energy, Noise Energy and Frequency        
+        
+        % for 2 terms
+        z2 = [rxx(1)/2;1] ;
+        % for 3 terms
+        z = [rxx(1)/2;rxx(1)/2;1] ;
+
         tau = 1 ;
-        for n=1:20
+        for n=1:5
+            % Get Jacobian for 2 terms
+            J = [cos(z2(2)*tau) -tau*z2(1)*sin(z2(2)*tau);...
+                cos(2*z2(2)*tau) -2*tau*z2(1)*sin(2*z2(2)*tau)] ;
+
+            % Update solution for 2 terms
+            z2 = z2 + pinv(J)* ...
+                (-[z2(1)*cos(z2(2)*tau)-rxx(2);z2(1)*cos(2*z2(2)*tau)-rxx(3)]) ;
+
             % Get Jacobian
-            J = [cos(z(2)*tau) -tau*z(1)*sin(z(2)*tau);...
-                cos(2*z(2)*tau) -2*tau*z(1)*sin(2*z(2)*tau)] ;
+            J1 = [1 1 0;...
+                cos(z(3)*tau) 0 -tau*z(1)*sin(z(3)*tau);...
+                cos(2*z(3)*tau) 0 -2*tau*z(1)*sin(2*z(3)*tau)] ;
 
             % Update solution
-            z = z + pinv(J)* ...
-                (-[z(1)*cos(z(2)*tau)-rxx(2);z(1)*cos(2*z(2)*tau)-rxx(3)]) ;
+            z = z + pinv(J1)* ...
+                (-[z(1)+z(2)-rxx(1);z(1)*cos(z(3)*tau)-rxx(2);z(1)*cos(2*z(3)*tau)-rxx(3)]) ;
         end
-
-        %[z,[txx(1);n2*N;2*pi*fsig/16368]]
 
         %fprintf('Frequency: %f\n', mod(z(2)*16368/2/pi,16368/2)) ;
         %fprintf('Signl Enr: %f\n', z(1)/N) ;
         
-        w_delta(i) = w_delta(i) + (fsig - mod(z(2)*16368/2/pi,16368/2))^2 ;
-        A_delta(i) = A_delta(i) + (z(1)/N - A^2/2)^2;
+        mod(z2(2)*16368/2/pi,16368/2)
+        w_delta(i) = w_delta(i) + (fsig - mod(z2(2)*16368/2/pi,16368/2))^2 ;
+        A_delta(i) = A_delta(i) + (z2(1)/N - A^2/2)^2;
+        
+        mod(z(3)*16368/2/pi,16368/2)
+        w_3_delta(i) = w_3_delta(i) + (fsig - mod(z(3)*16368/2/pi,16368/2))^2 ;
+        A_3_delta(i) = A_3_delta(i) + (z(1)/N - A^2/2)^2;
     end
     
     % normalie
     w_delta(i) = w_delta(i) / times ;
     A_delta(i) = A_delta(i) / times ;
     
+    w_3_delta(i) = w_3_delta(i) / times ;
+    A_3_delta(i) = A_3_delta(i) / times ;
+    
 end % for i=n(1):n(end)
 
 if A>0
     figure(1), grid on,
         subplot(2,1,1),
-            plot(n2, w_delta, '-mo'),
+            plot(n2, w_delta, '-mo', n2, w_3_delta, '-bx'),
             title('freq estimation variance with signal'),
-            grid on, xlabel('SNR'), ylabel('Variance'),        
+            grid on, xlabel('SNR'), ylabel('Variance'),
+            legend('2 terms', '3 terms'),
         subplot(2,1,2),
-            plot(n2, A_delta, '-bx'),
+            plot(n2, A_delta, '-mo', n2, A_3_delta, '-bx'),
             title('Energy estimation variance with signal'),
             grid on, xlabel('SNR'), ylabel('Variance'),
-            %legend('E', 'Est E'),
+            legend('2 terms', '3 terms'),
 else
     figure(1)
 
