0bf7f12e1a2e5f2de9fd05ceb667dca0959d9b5d
Alex Nikiforov <nikiforov.alex@rf-lab.org>
2013-04-07 23:35:06 +0400

    tests: 1 equation: fix a little bit

--- src/tests/newton/test_for_article/test_1_eq.m
+++ src/tests/newton/test_for_article/test_1_eq.m
@@ -9,7 +9,7 @@ times = 1 ;% number of simulation times
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 N = 16368 ;
-fsig = 3000 ;
+fsig = 4000 ;
 
 % storage for variance
 w_delta = zeros(numel(n2), 1) ;
@@ -30,21 +30,22 @@ for i=1:numel(n2)
         % Signal Energy, Noise Energy and Frequency        
         
         % for 2 terms
-        z2 = [rxx(1)/2;1000/16368*2*pi] ;
+        % z2 = [1000/16368*2*pi] ;
+        z2 = [1] ;
         
         tau = 1 ;
         for n=1:10
-            num = rxx(1)*cos(z2(2)*tau2) - rxx(2)*cos(z2(2)*tau1) ;
-            denom = -tau2*rxx(1)*sin(z2(2)*tau2) + tau1*rxx(2)*sin(z2(2)*tau1);
+            num = rxx(1)*cos(z2(1)*tau2) - rxx(2)*cos(z2(1)*tau1) ;
+            denom = -tau2*rxx(1)*sin(z2(1)*tau2) + tau1*rxx(2)*sin(z2(1)*tau1);
             
-            z2(2) = z2(2) - num/denom ;
+            z2(1) = z2(1) - num/denom ;
         end
 
-        freq = mod(z2(2)*16368/2/pi,16368/2);
-        fprintf('Frequency: %.2f E=%.2f\n', freq, rxx(1) / cos(z2(2)*tau1) / N) ;        
+        freq = mod(z2(1)*16368/2/pi,16368/2);
+        fprintf('Frequency: %.2f E=%.2f\n', freq, rxx(1) / cos(z2(1)*tau1) / N) ;        
         
-        w_delta(i) = w_delta(i) + (fsig - mod(z2(2)*16368/2/pi,16368/2))^2 ;
-        A_delta(i) = A_delta(i) + (rxx(2) / cos(z2(2)) / N - A^2/2)^2;
+        w_delta(i) = w_delta(i) + (fsig - mod(z2(1)*16368/2/pi,16368/2))^2 ;
+        A_delta(i) = A_delta(i) + (rxx(2) / cos(z2(1)*tau1) / N - A^2/2)^2;
     end
     
     % normalie
