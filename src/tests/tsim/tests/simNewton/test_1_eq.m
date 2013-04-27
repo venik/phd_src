b54fb6fa6b3b6e1168a88d364d32201e492d5426
Alex Nikiforov <nikiforov.alex@rf-lab.org>
2013-04-27 16:30:56 +0400

    tests: Newton: add compare analityc and Newton for 1 equation

--- src/tests/tsim/tests/simNewton/test_1_eq.m
+++ src/tests/tsim/tests/simNewton/test_1_eq.m
@@ -3,9 +3,9 @@ clc, clear all ;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Parameters for define
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-n2 = 15 ; % !!! in SNR dB
+n2 = -5:15 ; % !!! in SNR dB
 A = 1 ;     % sine amplitude
-times = 1 ;% number of simulation times
+times = 100 ;% number of simulation times
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 N = 16368 ;
@@ -15,10 +15,14 @@ fsig = 3000 ;
 w_delta = zeros(numel(n2), 1) ;
 A_delta = zeros(numel(n2), 1) ;
 
+% analitycal data storage
+wa_delta = zeros(numel(n2), 1) ;
+Aa_delta = zeros(numel(n2), 1) ;
+
 tau1 = 1 ;
 tau2 = 2 ;
 
-Nnw = 15 ;
+Nnw = 5 ;
 z2 = zeros(Nnw, 1) ;
 
 for i=1:numel(n2)
@@ -33,7 +37,7 @@ for i=1:numel(n2)
         % Signal Energy, Noise Energy and Frequency        
         
         % for 1 term
-         z2(1) = [2850/16368*2*pi] ;
+         z2(1) = [2000/16368*2*pi] ;
         %z2 = [1] ;
         
         tau = 1 ;
@@ -49,27 +53,43 @@ for i=1:numel(n2)
         
         w_delta(i) = w_delta(i) + (fsig - mod(z2(end)*16368/2/pi,16368/2))^2 ;
         A_delta(i) = A_delta(i) + (rxx(1) / cos(z2(end)*tau1) / N - A^2/2)^2;
-    end
+        
+        % analitycal solve
+        D = rxx(2)^2 + 8*rxx(1)^2 ;
+        y1 = (rxx(2) + sqrt(D)) / (4*rxx(1)) ;
+        y2 = (rxx(2) - sqrt(D)) / (4*rxx(1)) ;
+
+        %2*rxx(1)*y1^2 - rxx(2)*y1 - rxx(1) ;
+        %2*rxx(1)*y2^2 - rxx(2)*y2 - rxx(1) ;
+
+        freq11 = 0;
+        freq12 = 0;
+        if tau1 == 1
+            if (y1>1)
+                y1 = y2 ;
+            end
+            freq11 = mod(acos(y1)*16368/2/pi,16368/2) ;
+        else
+            freq11 = mod(acos(y1)*16368/2/pi,16368/2) ;
+            freq12 = mod(acos(y2)*16368/2/pi,16368/2) ;
+        end
+
+        fprintf('Frequency analytic: %.2f %.2f E=%.2f\n', freq11, freq12, rxx(1) / y1 / N) ;
+        
+        wa_delta(i) = wa_delta(i) + (fsig - mod(acos(y1)*16368/2/pi,16368/2))^2 ;
+        Aa_delta(i) = Aa_delta(i) + (rxx(1) / y1 / N - A^2/2)^2 ;
+
+    end %    for j=1:times
     
     % normalie
     w_delta(i) = w_delta(i) / times ;
     A_delta(i) = A_delta(i) / times ;
+    
+    wa_delta(i) = wa_delta(i) / times ;
+    Aa_delta(i) = Aa_delta(i) / times ;
 
 end % for i=n(1):n(end)
 
-D = rxx(2)^2 + 8*rxx(1)^2 ;
-y1 = (rxx(2) + sqrt(D)) / (4*rxx(1)) ;
-y2 = (rxx(2) - sqrt(D)) / (4*rxx(1)) ;
-
-if (y1<1)
-    yy = y1 ;
-else
-    yy = y2 ;
-end
-
-freq11 = mod(acos(yy)*16368/2/pi,16368/2) ;
-        
-fprintf('Frequency analytic: %.2f E=%.2f\n', freq11, rxx(1) / yy / N) ;        
 
 % dont want to plot a point
 if numel(n2) == 1
@@ -87,10 +107,15 @@ if numel(n2) == 1
 
     hold off, plot(alpha/2/pi*16368, gamma1, 'LineWidth', 2) ;
     hold on, plot(fsig, 0, '^','Color',[.3 0.5 0.3],'MarkerSize',10,'LineWidth',2) ;
-    hold on, plot(z2(:)/2/pi*16368, A_vals, 'g-+','Color',[.8 0.1 0.1],'LineWidth',1) ;
-    xlim([0 8000]), xlabel('Частота', 'Fontsize', 14), ylabel('A', 'Fontsize', 14) ; 
-    title(sprintf('Итерации поиска решения нелинейного уравнения 12 смещения %d и %d', tau1, tau2), ...
+    hold on, plot([freq11; freq12], 0, '*','Color',[1 0.0 0.0],'MarkerSize',10,'LineWidth',2) ;
+    hold on, plot(z2(:)/2/pi*16368, zeros(numel(z2), 1), 'g-+','Color',[.8 0.1 0.1],'LineWidth',1) ;
+    xlim([0 8000]), xlabel('Частота, Гц', 'Fontsize', 14), ylabel('A', 'Fontsize', 14) ; 
+    title(sprintf('Итерации поиска решения нелинейного уравнения 14, смещения %d и %d', tau1, tau2), ...
             'Fontsize', 14);
+    legend('Функция', 'Частота сигнала', ...
+            'Оценка частоты аналитическим методом', ...
+            'Оценка частоты аналитическим методом', ...
+            'Оценка частоты методом ньютона')
     grid on ;
 
     return;
@@ -100,13 +125,15 @@ end
 if A>0
     figure(1), grid on,
         subplot(2,1,1),
-            plot(n2, w_delta, '-mo'),
+            plot(n2, w_delta, '-mo', n2, wa_delta, '-rx'),
             title('Оценка частоты сигнала при его присутствии для уравнения 12', 'Fontsize', 14),
             grid on, xlabel('ОСШ', 'Fontsize', 14), ylabel('СКО', 'Fontsize', 14)
+            legend('Ньютон', 'Аналитическое решение');
         subplot(2,1,2),
-            plot(n2, A_delta, '-mo'),
+            plot(n2, A_delta, '-mo', n2, Aa_delta, '-rx'),
             title('Оценка энергии сигнала при его присутствии для уравнения 12', 'Fontsize', 14),
-            grid on, xlabel('ОСШ', 'Fontsize', 14), ylabel('СКО', 'Fontsize', 14)
+            grid on, xlabel('ОСШ', 'Fontsize', 14), ylabel('СКО', 'Fontsize', 14),
+            legend('Ньютон', 'Аналитическое решение');
 else
     figure(1)
         subplot(2,1,1),
