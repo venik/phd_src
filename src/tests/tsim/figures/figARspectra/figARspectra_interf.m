d8f394e320fbc608c4ed43bdc913e769ff31029b
estel <melnikov.aleksey@rf-lab.org>
2013-08-12 18:20:53 +0400

    tsim: figure styles

---
+++ src/tests/tsim/figures/figARspectra/figARspectra_interf.m
@@ -0,0 +1,42 @@
+clc, clear ;
+% get access to model
+curPath = pwd() ;
+cd('..\\..\\model') ;
+modelPath = pwd() ;
+cd( curPath ) ;
+addpath(modelPath) ;
+
+% get default parameters
+init_rand(1) ;
+ifsmp = get_ifsmp() ;
+ifsmp.snr_db = 10 ;
+ifsmp.sats = [1,2] ;
+ifsmp.vars = [1.0 1.0] ;
+ifsmp.fs = [4670000 4100000 4093000  4092400] ;
+ifsmp.fd = 16368000 ;
+ifsmp.delays = [100,150,230,10] ;
+ifsmp.sigLengthMsec = 12 ;
+
+[~, x ,sats, delays, signoise] = get_gps_if_signal( ifsmp ) ;
+code = get_gps_ca_code(ifsmp.sats(1),ifsmp.fd,length(x)+delays(1)) ;
+y = x.*code(1+delays(1):length(x)+delays(1)) ;
+
+rxx = get_rxx(y, 0:2 ) ;
+b = ar_model([rxx(1); rxx(2); rxx(3)]) ;
+[poles, omega0, Hjw0] = get_ar_pole(b) ;
+omega = 0:0.02:pi ;
+Hjw = 1.0./( -b(2)*exp(-2j*omega) - b(1)*exp(-1j*omega) + 1.0 ) ;
+
+hold off, pwelch(x,4096) ;
+hold on, pwelch(y,4096) ;
+hold on, plot(omega/pi,10*log10(Hjw.*conj(Hjw)),'-.') ;
+
+ylabel('Спектральная плотность dB/rad/отсчет') ;
+xlabel('Нормализованная частота \pi/rad/отсчет') ;
+title('')
+legend('СПМ сигнала','СПМ сигнала после снятия ПСП','Частотный отклик АР-модели') ;
+phd_figure_style(gcf) ;
+
+
+% remove model path
+rmpath(modelPath) ;
---
+++ src/tests/tsim/model/phd_figure_style.m
@@ -0,0 +1,32 @@
+function phd_figure_style(figHandle)
+% set exportable image size
+%figHandle = gcf ;
+xSize = 640 ;
+ySize = 480 ;
+position = get(figHandle,'Position') ;
+set(gcf,'Position', ...
+    [position(1)+position(3)-xSize, position(2)+position(4)-ySize, ...
+    xSize, ySize]) ;
+figAxes = get(figHandle,'CurrentAxes') ;
+set(figAxes(1),'LineWidth',2) ;
+set(figAxes(1),'XGrid','on') ;
+set(figAxes(1),'YGrid','on') ;
+set(figAxes(1),'FontSize',14) ;
+
+colorTbl = [.75 0.33 0.33 ; .33 0.33 0.75 ; .4 0.7 0.55  ] ;
+
+figLines = findobj(figAxes(1),'Type','line') ;
+for n=1:length(figLines)
+    handl = figLines(n) ;
+    set(handl,'Color',colorTbl(n,:)) ;
+    set(handl,'LineWidth',2) ;
+end
+
+obj_list = findall(gca) ;
+for n=1:length(obj_list)
+    type = get(obj_list(n),'Type') ;
+    if strcmpi(type,'text')
+        set(obj_list(n),'FontSize',14) ;
+    end
+end
+
