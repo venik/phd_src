ac281d278b377d1ebcdb3f403c59b164c1a08ab1
Nikiforov Alex <nikiforov.alex@rf-lab.org>
2013-11-19 22:39:07 +0400

    test: tsim: fix model

--- src/tests/tsim/model/get_if_signal.m
+++ src/tests/tsim/model/get_if_signal.m
@@ -1,6 +1,10 @@
-function [x,y,sats, delays, signoise] = get_if_signal(ifsmp)
-N = 1023*15 ; % chips
-max_delay = 20 ; % chips
+function [x,y,sats, delays, signoise] = get_if_signal(ifsmp, ms)
+N = 1023*16 ; % chips
+%max_delay = 20 ; % chips
+
+if nargin < 2
+    ms = 1 ;
+end
 
 % IF signal model parameters
 sats = ifsmp.sats ;
@@ -10,13 +14,13 @@ fd = ifsmp.fd ;
 delays = ifsmp.delays ;
 snr_db = ifsmp.snr_db ;
 
-x = zeros(N*16,1) ;
+x = zeros(N*16*ms, 1) ;
 for k=1:numel(sats)
-    c = sqrt(vars(k)*2)*cos(2*pi*fs(k)/fd*(0:(N+max_delay)*16-1))' ;
-    code = get_ca_code16(N+max_delay,sats(k)) ;
-    code = code(:) ;
+    c = sqrt(vars(k)*2)*cos(2*pi*fs(k)/fd*(0:ms*N*16-1))' ;
+    code = repmat(get_ca_code16(N, sats(k)), ms, 1);
     tx = code.*c ;
-    tx = tx(1+delays(k):N*16+delays(k)) ;
+    %tx = tx(1+delays(k):N*16+delays(k)) ;
+    tx = circshift(tx, delays(k)) ;
     x = x + tx ;
 end
 if (snr_db>=100)
@@ -25,4 +29,5 @@ else
     signoise = 10^(-snr_db/10)*var(x) ;
 end
 %fprintf('if_signal_model: signoise:%f\n', signoise ) ;
-y = x + randn(size(x))*sqrt(signoise) ;
+sig = x + randn(size(x))*sqrt(signoise) ;
+y = sig(1: ms*N) ;
