1511754f1ca19be7fe1b5d1984c3272428749bf2
Nikiforov Alex <nikiforov.alex@rf-lab.org>
2013-11-20 21:42:00 +0400

    tests: tsim: improve signal model

--- src/tests/tsim/model/get_if_signal.m
+++ src/tests/tsim/model/get_if_signal.m
@@ -1,9 +1,12 @@
-function [x,y,sats, delays, signoise] = get_if_signal(ifsmp, ms)
+function [x,y,sats, delays, signoise] = get_if_signal(ifsmp, ms, multipath)
 N = 1023*16 ; % chips
 %max_delay = 20 ; % chips
 
 if nargin < 2
     ms = 1 ;
+end
+if nargin < 3
+    multipath = 0 ;
 end
 
 % IF signal model parameters
@@ -22,6 +25,20 @@ for k=1:numel(sats)
     %tx = tx(1+delays(k):N*16+delays(k)) ;
     tx = circshift(tx, delays(k)) ;
     x = x + tx ;
+    
+    % add one more ray for main signal
+    if k == 1 && multipath > 0
+        for kk = 1:multipath
+            E = 0.5 * rand() ;
+            phase = rand() * pi - pi ;
+            c = sqrt(E*2)*cos(2*pi*fs(k)/fd*(0:ms*N*16-1) + phase)' ;
+            tx = code.*c ;
+            delay_m = delays(k) + round(N*rand()) ;
+            tx = circshift(tx, delay_m) ;
+            x = x + tx ;
+            fprintf('add ray %d, E=%f, delay:%d\n', kk, E, delay_m) ;
+        end ; % kk
+    end ; % k == 1
 end
 if (snr_db>=100)
     signoise = 0 ;
