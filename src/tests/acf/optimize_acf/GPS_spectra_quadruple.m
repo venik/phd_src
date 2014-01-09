aac76db3ebd569814769effff034f46e98950337
Nikiforov Alex <nikiforov.alex@rf-lab.org>
2014-01-10 03:09:11 +0400

    tests: acf: fix it

--- src/tests/acf/optimize_acf/GPS_spectra_quadruple.m
+++ src/tests/acf/optimize_acf/GPS_spectra_quadruple.m
@@ -6,9 +6,9 @@ addpath(modelPath) ;
 % get default parameters
 % init_rand(1) ;
 ifsmp = get_ifsmp() ;
-ifsmp.snr_db = 100 ;
+ifsmp.snr_db = -20 ;
 ifsmp.sats = [1 2] ;
-ifsmp.vars = [1.0 1.0] ;
+ifsmp.vars = [1.0 0.0] ;
 ifsmp.fs = [4.090333e6, 4.093303e6] ;
 ifsmp.fd = 16.368e6 ;
 ifsmp.delays = [100,150,230,10] ;
@@ -31,14 +31,18 @@ y = x.*code(1+delays(1):length(x)+delays(1)) ;
 % y_new = y_new / kk ;
 y_new = y(1:N) ;
 
-Y = fft(y_new) ; 
+Y = fft(y_new - mean(y_new), 2^(nextpow2(length(y))+1)) ; 
 YY = Y .* conj(Y) ;
 %YY = YY ./ max(YY) ;
-%rss1 = ifft(YY) ;
-%rss2 = ifft(YY .^ 4) ;
-rss3 = ifft(YY .^ 8) ;
+rss1 = ifft(YY) ;
+rss2 = ifft(YY .^ 4) ;
+rss3 = ifft(YY .^ 16) ;
 
-b = ar_model([rss3(1) ; rss3(2) ; rss3(3)]) ;
+% rss1 = rss1 ./ rss1(1) ;
+% rss_a = autocorr(y_new, length(y_new) - 1) ;
+% b_a = ar_model([rss_a(1) ; rss_a(2) ; rss_a(3)]) 
+
+b = ar_model([rss3(1) ; rss3(2) ; rss3(3)]) 
 
 [pole, omega0, Hjw0] = get_ar_pole(b) ;
 fprintf('freq:%8.2f\n', omega0*ifsmp.fd/2/pi) ;
