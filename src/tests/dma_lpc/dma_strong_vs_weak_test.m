06145cbb614bfaaef5ca4841651bbaf61f1e7b29
Nikiforov Alex <nikiforov.alex@rf-lab.org>
2014-03-05 14:13:01 +0400

    test: dma: strong vs weak

--- src/tests/dma_lpc/dma_strong_vs_weak_test.m
+++ src/tests/dma_lpc/dma_strong_vs_weak_test.m
@@ -12,12 +12,12 @@ fs = 16.368e6 ;
 f1 = 4.092e6;
 f2 = 4.095e6;
 a1 = 1 ;
-a2 = 1:-0.1:0.1 ;
-%a2 = 0.4;
+a2 = 1:-0.05:0.1 ;
 sigma = 0 ;
 tau_s2 = 2400 ;
 tau = 64 ;
 sig_length = 2*N-1 ;
+times = 1000 ;
 
 c1 = get_ca_code16( 1023, 1) ;
 c2 = get_ca_code16( 1023, 2) ;
@@ -26,47 +26,62 @@ c1 = repmat(c1, 2, 1) ;
 c2 = repmat(c2, 2, 1) ;
 
 res_a1a2 = zeros(length(a2), 1) ;
+match = zeros(length(a2), 1) ;
 
 for kk=1:length(a2)
-    s1 = a1*exp(1j*2*pi*f1/fs*(0:sig_length)) ;
-    s2 = a2(kk)*exp(1j*2*pi*f2/fs*(0:sig_length)) ;
+    for jj=1:times
+        s1 = a1*exp(1j*2*pi*f1/fs*(0:sig_length)) ;
+        s2 = a2(kk)*exp(1j*2*pi*f2/fs*(0:sig_length)) ;
 
-    s1 = c1.*s1.' ;
-    s2 = s2.*c2.' ;
-    s1 = s1(:) ;
-    s2 = s2(:) ;
+        s1 = c1.*s1.' ;
+        s2 = s2.*c2.' ;
+        s1 = s1(:) ;
+        s2 = s2(:) ;
 
-    s2 = circshift(s2, tau_s2) ;
-    noise = sqrt(sigma) * randn(length(s1), 1) ;
+        tau_s2 = round(rand() * 16368 + 8) ;
+        
+        % adjust corner cases
+        if (tau_s2 < 9)
+            tau_s2 = tau_s2 + 8;
+        elseif (tau_s2 > (16368-8))
+            tau_s2 = tau_s2 - 8;
+        end;
+        
+        s2 = circshift(s2, tau_s2) ;
+        noise = sqrt(sigma) * randn(length(s1), 1) ;
 
-    x = s1 + s2 +  noise ;
+        x = s1 + s2 +  noise ;
 
-    xx = x(1:N) .* conj(x(tau : tau + N - 1)) ;
-    XX = fft(xx) ;
+        xx = x(1:N) .* conj(x(tau : tau + N - 1)) ;
+        XX = fft(xx) ;
 
-    c2_new = c2(1:N) .* c2( 1 + tau : N + tau) ;
-    C2_NEW = fft(c2_new) ;
+        c2_new = c2(1:N) .* c2( 1 + tau : N + tau) ;
+        C2_NEW = fft(c2_new) ;
 
-    res = ifft(XX .* conj(C2_NEW)) ;
-    res = res .* conj(res) ;
-    
-    [res_val, res_pos] = max(res) ;
-    
-    if((res_pos > 2400 + 8) || (res_pos < 2400 - 8))
-        % fprintf('%0.2f: miss\n', a2(kk)) ;
-        res_a1a2(kk) = 0 ;
-    else
-        res_a1a2(kk) = 10*log10(res_val / std(res)) ;
-    end; % if 
-    
+        res = ifft(XX .* conj(C2_NEW)) ;
+        res = res .* conj(res) ;
+
+        [res_val, res_pos] = max(res) ;
+
+        if((res_pos > tau_s2 + 8) || (res_pos < tau_s2 - 8))
+            %fprintf('%0.2f: miss real tau:%d est tau:%d\n', a2(kk), tau_s2, res_pos) ;
+            %res_a1a2(kk) = 0 ;
+        else
+            match(kk) = match(kk) + 1 ;
+            %res_a1a2(kk) = 10*log10(res_val / std(res)) ;
+        end; % if 
+    end % jj 
+    match(kk) = match(kk) / times ;
 end % kk=1:length(a2)
 
 base_line = repmat(7, 1, length(res_a1a2));
-a2_dB = 10*log10(1./a2) ;
+a2_dB = 10*log10(a2./a1) ;
 
-plot(a2_dB, res_a1a2, '-rx', a2_dB, base_line),
-    legend('max / std', '7dB line') ;
-    xlabel('A1/A2 dB')
+%plot(a2_dB, res_a1a2, '-kx', a2_dB, base_line, '-ko'),
+%    legend('max / std', '7dB line') ;
+%    xlabel('A1/A2 dB')  
+
+plot(a2_dB, match, '-kx')
 
 %plot(res)
 
