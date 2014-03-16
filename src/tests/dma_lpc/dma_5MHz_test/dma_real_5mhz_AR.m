55854ad51807df7d17fb82c0b2457447ac1b5a5e
Nikiforov Alex <nikiforov.alex@rf-lab.org>
2014-03-16 09:08:04 +0400

    src: tests: dma: cleanup the code

--- src/tests/dma_lpc/dma_5MHz_test/dma_real_5mhz_AR.m
+++ src/tests/dma_lpc/dma_5MHz_test/dma_real_5mhz_AR.m
@@ -4,13 +4,12 @@ delta1= 10; %дельта смещения
 fs =5456e3 ;
 freq = 4.092e6 ;
 N = 5456 ;
-secs=5;
 mi_sec = 0;
 PRN= 30;
 
 otstup = 70000;
 
-y = load_primo_file('101112_0928GMT_primo_fs5456_fif4092.dat',5456*200);  %обработали принятый сигнал
+y = load_primo_file('101112_0928GMT_primo_fs5456_fif4092.dat',5456*200);
 y = double (y);   
 
 cacode2= CACode(PRN);
@@ -24,22 +23,14 @@ end
 
 %%%%%%%%%
 % DMA part
+CA_lo = repmat(CAcode16, 1, 2) ;
+sig_new = zeros(N, 1);
 
-CA_lo = repmat(CAcode16, 1, secs) ;
+for jj=0:8
+    sig_new = sig_new + y(otstup + (N * jj) : N + otstup + (N * jj) - 1) .* y(otstup+ delta1+(N * jj) : N + delta1 + otstup + (N * jj)-1) ;
+end;
 
-summ1 = y(otstup+(N*mi_sec):N+otstup+(N*mi_sec)-1) .* y(otstup+ delta1+(N*mi_sec):N+delta1+otstup+(N*mi_sec)-1) ;
-summ2 = y(otstup+(N*(mi_sec+1)):N+(N*(mi_sec+1))+otstup-1) .* y(otstup+ delta1+(N*(mi_sec+1)):N+delta1+otstup+(N*(mi_sec+1))-1);
-summ3 = y(otstup+(N*(mi_sec+2)):N+(N*(mi_sec+2))+otstup-1) .*  y(otstup+ delta1+(N*(mi_sec+2)):N+delta1+otstup+(N*(mi_sec+2))-1);
-summ4 = y(otstup+(N*(mi_sec+3)):N+(N*(mi_sec+3))+otstup-1) .*  y(otstup+ delta1+(N*(mi_sec+3)):N+delta1+otstup+(N*(mi_sec+3))-1);
-summ5 = y(otstup+(N*(mi_sec+4)):N+(N*(mi_sec+4))+otstup-1) .*  y(otstup+ delta1+(N*(mi_sec+4)):N+delta1+otstup+(N*(mi_sec+4))-1);
-summ6 = y(otstup+(N*(mi_sec+5)):N+(N*(mi_sec+5))+otstup-1) .*  y(otstup+ delta1+(N*(mi_sec+5)):N+delta1+otstup+(N*(mi_sec+5))-1);
-summ7 = y(otstup+(N*(mi_sec+6)):N+(N*(mi_sec+6))+otstup-1) .*  y(otstup+ delta1+(N*(mi_sec+6)):N+delta1+otstup+(N*(mi_sec+6))-1);
-summ8 = y(otstup+(N*(mi_sec+7)):N+(N*(mi_sec+7))+otstup-1) .*  y(otstup+ delta1+(N*(mi_sec+7)):N+delta1+otstup+(N*(mi_sec+7))-1);
-summ9 = y(otstup+(N*(mi_sec+8)):N+(N*(mi_sec+8))+otstup-1) .*  y(otstup+ delta1+(N*(mi_sec+8)):N+delta1+otstup+(N*(mi_sec+8))-1);
-
-sig_new = summ1+summ2+summ3+summ4+summ5+summ6+summ7+summ8+summ9;
-
-new_CAcode16 = CA_lo(1:N) .* CA_lo(1 + delta1:N+delta1) ;
+new_CAcode16 = CA_lo(1:N) .* CA_lo(1 + delta1 : N + delta1) ;
 
 % скоррелировали  CA код спутника со сгенерированным кодом
 q = ifft(fft(new_CAcode16(1:N)) .* conj(fft(sig_new(1:N).')) ) ;
@@ -47,11 +38,10 @@ q = ifft(fft(new_CAcode16(1:N)) .* conj(fft(sig_new(1:N).')) ) ;
 acx = q.*conj(q);
 [value_x, index_x] = max(acx);
 
-fprintf('CA phase: %d', index_x);
+fprintf('PRN: %02d\tCA phase: %d\n', PRN, index_x);
 
 %%%%%%%%%
 % PLL part
-
 x  = y(otstup+(N*mi_sec)-(index_x-1):N+otstup+(N*mi_sec)-(index_x-1)-1).*CAcode16.';
 
 x_new = zeros(1,N*5);
@@ -80,17 +70,16 @@ settings = initSettings();
 ss = FileSource(settings.fileName, 'int8', 'r');
 
 tracker = Tracker(PRN, ss);
-tracker.Init(otstup+(N*mi_sec)-(index_x-1), ...
-					freq_z*1000);
+tracker.Init(otstup+(N*mi_sec)-(index_x-1), freq_z*1000);
                 
-proc_time=100;
+proc_time = settings.processTime;
                 
 I = zeros(proc_time, 1);
 Q = zeros(proc_time, 1);
 CarrierFrequency = 0;
 CodeError = zeros(proc_time, 1);
 CarrierError = zeros(proc_time, 1);
-time =1;
+time = 1;
 
 while true
     tracker.Execute();
@@ -111,4 +100,7 @@ while true
     end
 end
 
-plot(I);
+%plot(I);
+
+plot(CarrierError(1:200)),
+    xlabel('мс'), ylabel('ошибка');
