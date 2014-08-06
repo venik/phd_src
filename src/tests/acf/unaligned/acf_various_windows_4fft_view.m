909d238c8c49203785e679a94da94632f5ea11db
Alex Nikiforov <nikiforov.alex@rf-lab.org>
2014-08-06 01:22:16 -0700

    src: tests: acf: unaligned: 4 FFT proper view

--- src/tests/acf/unaligned/acf_various_windows_4fft_view.m
+++ src/tests/acf/unaligned/acf_various_windows_4fft_view.m
@@ -8,13 +8,33 @@ addpath(modelPath) ;
 
 load('acf_various_windows_4fft.mat');
 
-freq_plot = freq1 ;
-
-semilogy(SNR_dB, freq_plot(:,1), '-go', SNR_dB, freq_plot(:,2), '-b*', ...
-    SNR_dB, freq_plot(:,3), '-r+', SNR_dB, freq_plot(:,4), '-y+') ;        
-    title('SKO') ,
-    legend('1', '2', '3', '4', '5') ;          % legend('PLL line', 'Rect', 'Hamming', 'Blackman', 'Hann') ;
+figure(1), 
+semilogy(SNR_dB, freq1(:,1), '-go', SNR_dB, freq1(:,2), '-b*', ...
+    SNR_dB, freq1(:,3), '-r+', SNR_dB, freq1(:,4), '-y+') ;        
+    title('1 FFT SKO') ,
+    legend('1', '2', '3', '4') ;          % legend('PLL line', 'Rect', 'Hamming', 'Blackman', 'Hann') ;
     phd_figure_style(gcf) ;
-        
+    
+figure(2),
+semilogy(SNR_dB, freq2(:,1), '-go', SNR_dB, freq2(:,2), '-b*', ...
+    SNR_dB, freq2(:,3), '-r+', SNR_dB, freq2(:,4), '-y+') ;        
+    title('2 FFT SKO') ,
+    legend('1', '2', '3', '4') ;          % legend('PLL line', 'Rect', 'Hamming', 'Blackman', 'Hann') ;
+    phd_figure_style(gcf) ;
+   
+figure(3)
+semilogy(SNR_dB, freq3(:,1), '-go', SNR_dB, freq3(:,2), '-b*', ...
+    SNR_dB, freq3(:,3), '-r+', SNR_dB, freq3(:,4), '-y+') ;        
+    title('3 FFT SKO') ,
+    legend('1', '2', '3', '4') ;          % legend('PLL line', 'Rect', 'Hamming', 'Blackman', 'Hann') ;
+    phd_figure_style(gcf) ;
+
+figure(4)
+semilogy(SNR_dB, freq1(:,1), '-go', SNR_dB, freq2(:,2), '-b*', ...
+    SNR_dB, freq3(:,3), '-r+') ;        
+    title('Rect for 1, 2, 3') ,
+    legend('1', '2', '3') ;          % legend('PLL line', 'Rect', 'Hamming', 'Blackman', 'Hann') ;
+    phd_figure_style(gcf) ;    
+    
 % remove model path
 rmpath(modelPath) ;
