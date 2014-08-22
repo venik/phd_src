2cd55ceb8ac44d7fa1b2d7fd1af951e994dceac0
Nikiforov Alex <nikiforov.alex@rf-lab.org>
2014-08-22 13:11:49 +0400

    tests: dma: 5Mhz: classic approach. fix

--- src/tests/dma_lpc/dma_5MHz_test/classic_5mhz_tracking.m
+++ src/tests/dma_lpc/dma_5MHz_test/classic_5mhz_tracking.m
@@ -11,65 +11,14 @@ N = 5456 ;
 %PRN = 30;
 PRN = 31;
 
-%otstup = 70000 - 3645;
-otstup = 2000;
-
-y_base = load_primo_file('101112_0928GMT_primo_fs5456_fif4092.dat',N*200);
-y_base = double(y_base);
-
-y = y_base(otstup : otstup + N - 1) ;
-
-freq = 4.092e6-5e3 : 1e3 : 4.092e6+5e3;
-
-fs = 5.456e6 ;
-N = 5456 ;
-
-acx = 0 ;
-ca_phase = 0 ;
-freq_z = 0 ;
-
-res = zeros(N, 1) ;
-
-%%%%%%%
-% Correlator
-
-Y = fft(y);
-cacode2= CACode(PRN);
-CAcode1 = cacode2.Bits;  %генерируем CA код
-CAcode16 = zeros(1, N);  %объявили сигнал Синус* на CA код 
-
-for i=1:N
-    CAcode16(i) = CAcode1(ceil(1023000/fs*i));  
-end
-
-for p=1:length(freq)   
-    cos_opor = exp(-1i*2*pi*freq(p)/fs * (0:N-1));
-
-    lo_sig = CAcode16 .* cos_opor;
-
-    LO_SIG = fft(lo_sig);
-
-    q = ifft(LO_SIG .* conj(Y).') ;
-
-    acx_res = q .* conj(q) ;
-    [value_x, index_x] = max(acx_res) ;
-
-    if (value_x > acx)
-        res = acx_res ;
-        acx = value_x ;
-        ca_phase = index_x ;
-        freq_z = freq(p) ;
-    end;
-
-end; % p
-    
-fprintf('PRN: %02d\tCA phase: %d\tfreq: %.2f\tE/sigma: %.2f dB\n', ...
-    PRN, ca_phase, freq_z, 10*log10(acx / std(res)));
+otstup = 100;
 
 %%%%%%%
 % Fine freq
 settings = initSettings();
 ss = FileSource(settings.fileName, 'int8', 'r');
+
+ss.Move(otstup);
 
 detector = Detector(PRN, ss);
 detector.Execute();
